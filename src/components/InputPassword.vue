d13dbac92bf7394288913e174865eca02fc6b1c4
RenanJochem98 <renanjochem98@gmail.com>
2021-03-30 16:38:01 -0300

    Tentando isolar campo de senha

---
+++ src/components/InputPassword.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-input
+    v-model="password"
+    filled :type="isPwd ? 'password' : 'text'"
+    placeholder="Digite a sua senha"
+    lazy-rules
+    :rules="[ val => val && val.length > 0 || 'O valor digitado não é válido.']"
+    value=""
+    >
+    <template v-slot:append>
+    <q-icon
+    :name="isPwd ? 'visibility_off' : 'visibility'"
+    class="cursor-pointer"
+    @click="isPwd = !isPwd"
+    />
+    </template>
+  </q-input>
+</template>
+<script>
+
+export default {
+  name: 'InputPassword',
+  data () {
+    return {
+      isPwd: true,
+      password: ''
+    }
+  }
+}
+</script>
