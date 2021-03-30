f2c4d0d0ff12d0df5ee7fadd67d8bb3d9e745e20
RenanJochem98 <renanjochem98@gmail.com>
2021-03-30 16:38:50 -0300

    Criando layout para testes

---
+++ src/layouts/InicioLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="color-default text-white">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          icon="arrow_back"
+          to="/"
+        />
+        <q-toolbar-title>
+          Quero participar
+        </q-toolbar-title>
+        <BtnLogout />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="color-default text-white">
+      <q-toolbar>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<style>
+  .color-default{
+    background-color: #6A1B9A;
+  }
+</style>
+
+<script>
+import BtnLogout from 'src/components/BtnLogout.vue'
+export default {
+  components: { BtnLogout },
+  name: 'InicioLayout'
+}
+</script>
