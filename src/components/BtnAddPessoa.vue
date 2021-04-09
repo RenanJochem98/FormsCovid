bc2fb4297ad485af9d37041a8be623a4edc7f829
RenanJochem98 <renanjochem98@gmail.com>
2021-04-09 11:11:05 -0300

    Dando inicio a página apos login e criando componente de add pessoa

---
+++ src/components/BtnAddPessoa.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class='row template'>
+    <q-btn class="color">
+    <div>
+      <div>{{ titulo }}</div>
+      <div class='descricao'>{{ descricao }}</div>
+    </div>
+    <q-icon right size="3em" name="group_add" class="icon" />
+  </q-btn>
+  </div>
+</template>
+<style scoped>
+  .template {
+    padding: 1%;
+    margin: 0.1%;
+  }
+  .icon {
+    color: #6A1B9A;
+    font-size: 75%;
+  }
+  .btn {
+    background-color: rgb(241, 240, 240);
+  }
+  .descricao {
+    float: left;
+    font-size: 80%;
+  }
+</style>
+<script>
+export default {
+  name: 'BtnAddPessoa',
+  props: ['titulo', 'descricao']
+}
+</script>
--- src/pages/Inicio.vue
+++ src/pages/Inicio.vue
@@ -1,16 +1,48 @@
 <template>
   <div>
-    <h4>Inicio</h4>
-    <h5>{{ token }}</h5>
+    <h4>Olá, {{ nome }}!</h4>
+    <div>O gerenciamento da sua família acontece por aqui, é só você incluir uma nova pessoa</div>
+    <BtnAddPessoa titulo='Crianças' descricao='De 6 a 12 anos' />
+    <BtnAddPessoa titulo='Adolescentes' descricao='De 13 a 18 anos' />
+    <div>Sua familia</div>
+    <div v-for='crianca in this.criancas' :key="crianca.id" >
+      {{ crianca.name }}
+    </div>
   </div>
 </template>
 
 <script>
+import BtnAddPessoa from 'components/BtnAddPessoa'
+import api from 'src/services/api'
 export default {
   name: 'Inicio',
+  components: {
+    BtnAddPessoa
+  },
   data () {
     return {
-      token: this.$store.getters['login/getToken']
+      token: this.$store.getters['login/getToken'],
+      nome: 'Ciclana',
+      criancas: []
+    }
+  },
+  beforeMount () {
+    // busca nome
+    this.criancas = this.buscarCriancas()
+  },
+  methods: {
+    async buscarCriancas () {
+      console.log('Chegou aqui')
+      console.log(this.$store.getters['login/getToken'])
+      await api.get('/children/', {
+        headers: { Authorization: 'Token ' + this.$store.getters['login/getToken'] }
+      })
+        .then((response) => {
+          console.log(response)
+        }
+        ).catch((error) => {
+          console.log(error)
+        })
     }
   }
 }
