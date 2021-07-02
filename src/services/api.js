59b7f325e12ae3eeb7ba41c189af0ccd85331836
RenanJochem98 <renanjochem98@gmail.com>
2021-07-02 17:24:24 -0300

    Corrigindo url base da api e comentando logs

--- src/services/api.js
+++ src/services/api.js
@@ -10,17 +10,20 @@ function showCustom () {
     timeout: 1000
   })
 }
-axios.defaults.baseURL = 'https://covid-criancas.herokuapp.com/api/v1'
+axios.defaults.baseURL = 'http://localhost:8000/api/v1'
 
 const api = axios.create({
   timeout: 30000,
   headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
+    'Content-Type': 'application/json'
   }
 })
 
 api.interceptors.request.use(function (request) {
+  // console.log(request)
+  // request.headers['Access-Control-Allow-Origin'] = '*'
+  // request.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  // request.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE'
   showCustom()
   return request
 }, function (error) {
@@ -28,9 +31,8 @@ api.interceptors.request.use(function (request) {
 })
 
 api.interceptors.response.use(function (response) {
-  console.log('mensagem de response aqui')
-  console.log(response)
-  response.headers['Access-Control-Allow-Origin'] = '*'
+  // console.log('mensagem de response aqui')
+  // console.log(response)
   return response
 }, function (error) {
   return Promise.reject(error)
